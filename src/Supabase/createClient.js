24af4604e32886ec9b96eba09195f27304993140
bren112 <brenomarinho1610@gmail.com>
2025-09-15 15:32:11 -0300

    database

--- src/Supabase/createClient.js
+++ src/Supabase/createClient.js
@@ -1,6 +1,10 @@
 import {createClient} from "@supabase/supabase-js";
 
-export const supabase= createClient(
+/*export const supabase= createClient(
     "https://onvizcnnptpdimwyghsg.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9udml6Y25ucHRwZGltd3lnaHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0NzczMDIsImV4cCI6MjA3MjA1MzMwMn0.D34WzAKwk4TLSW0dP7fI0Jyya18mgq1TGb2ATT0Gbdg"
+    )*/
+export const supabase= createClient(
+    "https://jrbpwisclowinultbehj.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpyYnB3aXNjbG93aW51bHRiZWhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjU3NTc4NjMsImV4cCI6MjA0MTMzMzg2M30.dFbRcWi0v7zdiR58ZcuTNfMTzi_GKg_56VL1-UT2JYs"
     )
